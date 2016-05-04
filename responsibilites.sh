fcb1398d98dd2aebf889a2466a02c023a488c0d5
cs246 <cs246@cs246.(none)>
2016-05-04 11:36:56 -0700

    devops responsbilites

---
+++ responsibilites.sh
@@ -0,0 +1,17 @@
+The following are the responsibilities of DevOps team:
+
+   --> Build features iteratively
+   --> Collect metrics
+   --> Manage risk
+   --> Solve theory arguments with execution
+   --> Choose tools that fit the job
+   --> Integrate and deliver continuously
+   --> Write tests
+   --> Focus on availability
+   --> Plan for capacity
+   --> Practice incident response
+   --> Use scalable systems design
+   --> Be unique
+   --> Empower teams
+   --> Build consensus on important decisions
+   --> Put application and infrastructure through the same workflow
